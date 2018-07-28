0fbfa59d61045b8dbe12bfa305a62070f2ec146f
TripleToad <37130737+TripleToad@users.noreply.github.com>
2018-07-28 18:37:30 -0500

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -16,8 +16,10 @@ public class Controller {
 	}
 	
 	/**
-	 * Add a button for this controller to record
-	 * @param value
+	 * Add a button for this controller to record <br>
+	 * Values correspond to KeyEvent virtual keyboard values. To find these, <br>
+	 * look for VK_<keyname> constants in the KeyEvent documentation.
+	 * @param value corresponding to KeyEvent's virtual keyboard
 	 */
 	public void add(String name, int value) {
 		if(button_mappings.get(name) == null && button_values.get(value) == null) {
@@ -30,7 +32,10 @@ public class Controller {
 	
 	/**
 	 * Add many buttons for this controller to record
-	 * @param values
+	 * Values correspond to KeyEvent virtual keyboard values. To find these, <br>
+	 * look for VK_<keyname> constants in the KeyEvent documentation.
+	 * @param names a list of the names for key bindings, like "jump" and "move left"
+	 * @param values the key values corresponding to the names
 	 */
 	public void add(String[] names, int ... values) {
 		if(names.length != values.length) {
@@ -41,14 +46,31 @@ public class Controller {
 		}
 	}
 	
+	/**
+	* Determines if this controller has a name assigned to this key value.
+	* @param the key value
+	* @return true means it does, false means it does not
+	*/
 	public boolean contains(int keyval) {
 		return button_values.containsKey(keyval);
 	}
 	
+	/**
+	* Determines if there is a key value assigned to this name.
+	* @param name the name
+	* @return true means it does, false means it does not
+	*/
 	public boolean contains(String name) {
 		return button_mappings.containsKey(name);
 	}
 	
+	/**
+	* This function allows direct control of the values sent out by the Controller. <br>
+	* This may be useful in developing computer controlled players, or for debugging.
+	* @param button the key value to simulate having this value
+	* @param value the value to simulate the button having
+	* @return true if the controller is recording this key, false if it is not.
+	*/
 	public boolean set(int button, boolean value) {
 		// If the button is being recorded by this Controller, set its value
 		if(button_values.get(button) != null) {
@@ -58,6 +80,11 @@ public class Controller {
 		return false;
 	}
 	
+	/**
+	* Get the key value assigned to this name
+	* @param name the name
+	* @return the key value
+	*/
 	public boolean get(String name) {
 		return button_values.get(button_mappings.get(name));
 	}
